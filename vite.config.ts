e2acfe2ffdddecbde3e326db926e00a7de12bdb9
Lancelot de Ferrière <wraitii@gmail.com>
2022-04-27 11:24:37 +0200

    Vite4 + Vue3 + Typescript 5

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+const injectNpmVersion = () => {
+    return {
+        name: 'inject-npm-version',
+        config: () => ({ define: { 'import.meta.env.VERSION': JSON.stringify(process.env.npm_package_version) } }),
+    };
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        target: 'es2020',
+        sourcemap: true,
+        minify: mode === 'production',
+    },
+    plugins: [vue(), injectNpmVersion()],
+}));
