fbcc0a4866cd8dc0b9894837898225ed20e3f0a0
gaetbout <gaetan.ansotte@hotmail.com>
2023-04-02 20:39:06 +0200

    showing address

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,30 @@
 
 <template>
     <div class="flex justify-center">
-        <button  @click=" connect()">Connect wallet</button>
+        <button @click=" handle_connect()">Connect wallet</button>
     </div>
+    <div v-if="result">Result: {{ result.selectedAddress }}</div>
 </template>
 
-<script setup lang="ts">
-import { connect, disconnect } from "get-starknet"
+<script lang="ts">
+    import { connect } from "get-starknet"
+
+
+    export default {
+        data() {
+            return {
+                result: null
+            }
+        },
+        methods: {
+            async handle_connect() {
+                this.result = await connect();
+                console.log(this.result.starknet);
+            }
+        }
+    }
+
+
 </script>
 
 <style>
