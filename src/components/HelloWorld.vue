81b0eb9213f40febdc6ea14efedf181ae8357c6d
gaetbout <gaetan.ansotte@hotmail.com>
2023-04-03 21:36:22 +0200

    has guardian

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,24 +7,34 @@
         <div v-if="result">Result: {{ result.selectedAddress }}</div>
     </div>
     <div class="flex justify-center">
-        <button @click=" try_lama()">Escape wallet</button>
+        <button @click=" do_trigger_escape()">Escape wallet</button>
     </div>
 </template>
 
 <script setup lang="ts">
     import { connect } from "get-starknet"
     import { ref } from 'vue'
+    import { Contract, number } from "starknet";
+
 
     let result = ref(null);
 
     async function handle_connect() {
-        this.result = await connect();
-        console.log(this.result)
+        result.value = await connect();
+        console.log(result.value);
+        get_guardian();
     }
+    
+    // Check if there is no guardian ==> Nothing to do
 
-    // Check if there is no guardian ==> Free
+    async function get_guardian(){
+        const testAddress = result.value.selectedAddress;
 
-    async function try_lama() {
+        let a = await result.value.provider.callContract({contractAddress: testAddress, entrypoint:"getGuardian"});
+        console.log(a.result[0] != 0);
+    }
+    
+    async function do_trigger_escape() {
         await this.result.account.execute({
             contractAddress: this.result.selectedAddress,
             entrypoint: 'triggerEscapeGuardian'
