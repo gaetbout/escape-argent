42c12d2885d7041d73970a3d45801292f22cf03c
gaetbout <gaetan.ansotte@hotmail.com>
2023-04-21 19:56:57 +0200

    handle no argentX Wallet

--- src/components/ConnectButton.vue
+++ src/components/ConnectButton.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="flex justify-center items-center h-screen">
         <div class="text-center">
-            <h1 class="text-7xl font-bold p-10 ">Escape Argent</h1>
-            <button 
-                class="transition duration-300 ease-in-out hover:scale-110 text-white mb-4 py-4 px-10 rounded-full" 
-                @click=" handle_connect()"
-            >
-                <div class="text-3xl font-bold">
-                    Connect wallet 
-                    <ArgentLogo width="36" height="40" fill="#FFFFFF" />
-                </div>
-            </button>
+            <div v-if="error">
+                <h1 class="text-7xl font-bold p-10 ">No ArgentX wallet installed</h1>
+            </div>
+            <div v-else>
+                <h1 class="text-7xl p-5 font-bold">Escape Argent</h1>
+                <button 
+                    class="transition duration-300 ease-in-out hover:scale-110 text-white m-4 py-4 p-10 rounded-full" 
+                    @click=" handle_connect()"
+                >
+                    <div class="text-3xl font-bold">
+                        Connect wallet 
+                        <ArgentLogo width="36" height="40" fill="#FFFFFF" />
+                    </div>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -23,10 +28,12 @@
 
 <script setup lang="ts">
     // TODO Should I also put the Disconnect logic here?
+    import { ref } from 'vue'
     import sn from 'get-starknet-core'
     import ArgentLogo from '@/components/ArgentLogo.vue';
 
-    const emits = defineEmits(['connected'])
+    const emits = defineEmits(['connected']);
+    let error = ref(null);
 
     async function handle_connect() {
         const argent = (await sn.getAvailableWallets()).find(wallet => wallet.id === "argentX"  );
@@ -34,10 +41,10 @@
             sn.enable(argent).then( acc => {
                 emits('connected', acc)
             }).catch(() => {
-                console.log("You refused");
+                // Ignore
             });
         } else {
-            // TODO Handle this either no wallet or no argent.
+            error.value = true;
         }
     }
 </script>
